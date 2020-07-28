631e76bdf386fd8751273d8be52f8e012ddf4220
virchau13 <computernerd7777@gmail.com>
2020-07-28 20:55:08 +0800

    added autoupdate

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,7 @@ import * as os from 'os';
 import axios from 'axios';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as crypto from 'crypto';
 import { resolve } from 'path';
 import { rejects } from 'assert';
 
@@ -23,36 +24,40 @@ function makeSureDirExists(path: string): Promise<void> {
     });
 }
 
-function isExecutable(file: string): Promise<boolean> {
+function isValidFile(file: string | undefined): Promise<boolean> {
+    if(file === undefined) return promiseValue(false);
     return new Promise((res) => {
-        fs.access(file, fs.constants.X_OK, (err) => {
+        fs.access(file, fs.constants.X_OK | fs.constants.R_OK, (err) => {
             res(!err);
         });
     });
 }
 
-async function getExec(ctx: vscode.ExtensionContext) : Promise<string> {
+async function downloadPcse(ctx: vscode.ExtensionContext, url: string, filename: string): Promise<string> {
+    let fileRes = await axios.get(url, {responseType: 'stream'});
+    vscode.window.showInformationMessage("Downloading pcse...");
+    return new Promise((res, rej) => {
+        let pcsepath = path.join(ctx.globalStoragePath, filename);
+        let writeStream = fs.createWriteStream(pcsepath);
+        fileRes.data.pipe(writeStream);
+        writeStream.on('finish', () => {
+            vscode.window.showInformationMessage("Finished!");
+            fs.chmod(pcsepath, 0o777, () => 
+                res(pcsepath)
+            );
+        });
+        writeStream.on('error', err => {
+            rej('Could not finish download: ' + err);
+        })
+    });
+}
+
+async function getLatest(ctx: vscode.ExtensionContext) : Promise<any> {
     let releases = await axios.get('https://api.github.com/repos/virchau13/pcse/releases/latest');
     let assets: Array<any> = releases.data.assets;
     let asset = assets.find(el => el.name.indexOf(os.platform()) !== -1);
     if(asset){
-        let fileRes = await axios.get(asset.browser_download_url, {responseType: 'stream'});
-        await makeSureDirExists(ctx.globalStoragePath);
-        vscode.window.showInformationMessage("Downloading pcse...");
-        return new Promise((res, rej) => {
-            let pcsepath = path.join(ctx.globalStoragePath, asset.name);
-            let writeStream = fs.createWriteStream(pcsepath);
-            fileRes.data.pipe(writeStream);
-            writeStream.on('finish', () => {
-                vscode.window.showInformationMessage("Finished!");
-                fs.chmod(pcsepath, 0o777, () => 
-                    res(asset.name)
-                );
-            });
-            writeStream.on('error', err => {
-                rej('Could not finish download: ' + err);
-            })
-        });
+        return asset;
     } else {
         return Promise.reject('Incompatible OS. Please file a bug report, and I will get around to building a version for it.' +
         '(If you are on MacOS/Linux/Windows, this is a BUG, and you should report it.)');
@@ -60,41 +65,58 @@ async function getExec(ctx: vscode.ExtensionContext) : Promise<string> {
 }
 
 export function activate(context: vscode.ExtensionContext) {
-    let runId = 'pcse-ext.runPcse';
-	let cmd = vscode.commands.registerCommand(runId, () => {
-        new Promise<string>((res, rej) => {
-            let stored = context.globalState.get<string>('pcsepath');
-            res((stored !== undefined ? isExecutable(stored) : promiseValue(false)).then((isValid: boolean) : string | Promise<string> => {
-                if(!isValid){
-                    return getExec(context).then((name: string): string => path.join(context.globalStoragePath, name))
-                        .then((pcsepath: string) => context.globalState.update('pcsepath', pcsepath).then(() => pcsepath));
-                } else {
-                    return stored === undefined ? "" /* will never happen */ : stored;
+    new Promise((res) => {
+        let old = context.globalState.get<string>('pcsepath');
+        if(old !== undefined){
+            let name: string = old;
+            res(getLatest(context).then(latest_asset => {
+                if(latest_asset.name !== path.basename(name)){
+                    // new update
+                    return makeSureDirExists(context.globalStoragePath)
+                        .then(() => downloadPcse(context, latest_asset.browser_download_url, latest_asset.name))
+                        .then((pcsepath: string) => context.globalState.update('pcsepath', pcsepath));
                 }
             }));
-        }).then((pcsepath: string) => {
-            let ed = vscode.window.activeTextEditor;
-            if(ed && ed.document.fileName.endsWith('.pcse')){
-                if(terminal === undefined){
-                    terminal = vscode.window.createTerminal("pcse");
+        } else /* will be downloaded inside */ res();
+    }).then(() => {
+        let runId = 'pcse-ext.runPcse';
+        let cmd = vscode.commands.registerCommand(runId, () => {
+            new Promise<string>((res, rej) => {
+                let stored = context.globalState.get<string>('pcsepath');
+                res(isValidFile(stored).then((isValid: boolean) : string | Promise<string> => {
+                    if(!isValid){
+                        return makeSureDirExists(context.globalStoragePath)
+                            .then(() => getLatest(context))
+                            .then((asset: any) => downloadPcse(context, asset.browser_download_url, asset.name))
+                            .then((pcsepath: string) => context.globalState.update('pcsepath', pcsepath).then(() => pcsepath));
+                    } else {
+                        return stored === undefined ? "" /* will never happen */ : stored;
+                    }
+                }));
+            }).then((pcsepath: string) => {
+                let ed = vscode.window.activeTextEditor;
+                if(ed && ed.document.fileName.endsWith('.pcse')){
+                    if(terminal === undefined){
+                        terminal = vscode.window.createTerminal("pcse");
+                    }
+                    terminal.show();
+                    terminal.sendText(escapeStrTerm(pcsepath) + ' ' + escapeStrTerm(ed.document.fileName));
+                    
+                } else {
+                    vscode.window.showErrorMessage("Not a valid file!");
                 }
-                terminal.show();
-                terminal.sendText(escapeStrTerm(pcsepath) + ' ' + escapeStrTerm(ed.document.fileName));
-                
-            } else {
-                vscode.window.showErrorMessage("Not a valid file!");
-            }
-        }).catch(err => {
-            vscode.window.showErrorMessage(err);
-        })
+            }).catch(err => {
+                vscode.window.showErrorMessage(err);
+            })
+        });
+        context.subscriptions.push(cmd);
+    
+        runner = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+        runner.command = runId;
+        context.subscriptions.push(runner);
+        context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(onChangeBuffer));
+        onChangeBuffer(vscode.window.activeTextEditor);
     });
-    context.subscriptions.push(cmd);
-
-    runner = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-    runner.command = runId;
-    context.subscriptions.push(runner);
-    context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(onChangeBuffer));
-    onChangeBuffer(vscode.window.activeTextEditor);
 }
 
 function escapeStrTerm(str: string) : string {
